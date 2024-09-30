70d5aa324e27e5f9c8875acdd4c965e70ba1cf94
a6iyyu <ffst4irs@gmail.com>
2024-09-30 12:25:30 +0700

    :construction: Almost finish build the home page.

--- src/common/hamburger-menu.tsx
+++ src/common/hamburger-menu.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 export const HamburgerMenu: React.FC = () => {
   return (
     <section className="hamburger-menu flex h-full w-3/5 items-center justify-end lg:hidden">
-      <i className="fa-solid fa-bars cursor-pointer text-2xl text-gray-50" />
+      <i className="fa-solid fa-bars cursor-pointer text-2xl text-slate-950" />
     </section>
   );
 };
