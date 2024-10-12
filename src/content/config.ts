eebfa5d486da821970f33b062949172dfa91edd9
a6iyyu <ffst4irs@gmail.com>
2024-10-12 21:44:34 +0700

    :construction: Creating articles and repairing the carousel.

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ export const collections = {
   "blog": defineCollection({
     type: "content",
     schema: z.object({
-      title: z.string().trim().min(1, { message: "Title must be at least 1 character!" }).max(200, { message: "Title must be at most 125 characters!" }).regex(/^[A-Za-z0-9\s]+$/, { message: "Title can only contain letters, numbers, and spaces!" }),
+      title: z.string().trim().min(1, { message: "Title must be at least 1 character!" }).max(200, { message: "Title must be at most 125 characters!" }),
       description: z.string().trim().min(1, { message: "Description must be at least 1 character!" }).max(300, { message: "Description must be at most 200 characters!" }),
       image: z.string().optional(),
       date: z.date({ message: "Invalid date!" }).refine(date => date <= new Date(), { message: "Date can't be in the future!" }),
